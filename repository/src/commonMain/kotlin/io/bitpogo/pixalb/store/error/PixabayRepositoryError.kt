a218bfdae3e86789812c03e1262a84d1f55d8f2c
Matthias Geisler <matthias.geisler@data4life.care>
2022-08-10 09:13:17 +0200

    Model Repository

---
+++ repository/src/commonMain/kotlin/io/bitpogo/pixalb/store/error/PixabayRepositoryError.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2022 Matthias Geisler (bitPogo) / All rights reserved.
+ *
+ * Use of this source code is governed by Apache v2.0
+ */
+
+package io.bitpogo.pixalb.store.error
+
+sealed class PixabayRepositoryError(
+    message: String? = null,
+    cause: Throwable? = null,
+) : RuntimeException(message, cause) {
+    class NoConnection : PixabayRepositoryError()
+    class UnsuccessfulRequest : PixabayRepositoryError()
+}
